b13548137a2e7c75c376321d6357bc9b8343da43
Eric Moroiu <ericmoroiu@gmail.com>
2026-03-14 16:04:34 +0200

    feat(api): add local Twilio WhatsApp webhook flow with manual ngrok setup

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { WhatsAppModule } from './whatsapp.module';
-import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [WhatsAppModule],
-  providers: [PrismaService],
 })
 export class AppModule {}
